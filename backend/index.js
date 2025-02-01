38559dbcb64ec3b85934ec56fb559263621de1b0
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-02-01 07:36:52 +0200

    Feat: Add connect to mongoose function

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"
+import { connectDB } from "./db/connectDB.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -9,5 +10,6 @@ app.get("/", (req, res) => {
     res.json({ message: "Socialize MERN" })
 })
 app.listen(PORT, () => {
+    connectDB()
     console.log("Listening on port", PORT)
 })
